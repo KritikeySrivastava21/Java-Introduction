9dfe3ba5882fd4a9cf308cefae6a58ee11565685
Kritikey Srivastava <139680156+KritikeySrivastava21@users.noreply.github.com>
2023-07-16 14:36:16 +0530

    Create Display.java

---
+++ Display.java
@@ -0,0 +1,35 @@
+/*
+Display Message
+
+Write a program to display a welcome message on the screen.
+
+Welcome Message:
+Welcome to Java course!
+Hello
+Ninja
+
+Output format:
+Print the welcome message.
+
+Note:-
+You don't need to return anything. Just print the desired output.
+
+Constraints:
+Time Limit: 1-sec
+
+Sample Output 1:
+Welcome to Java course!
+Hello
+Ninja
+Explanation Of Sample Input 1:
+Printed the welcome message.
+*/
+
+public class Solution {
+    public static void printDisplayMessage() {
+        // Write your code here
+        System.out.println("Welcome to Java course!");
+        System.out.println("Hello");
+        System.out.println("Ninja");
+    }
+}
